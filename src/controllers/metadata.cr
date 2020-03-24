f4d755aa880ce9869da75a33ef69c051d03bb6c5
Kim Burgess <kim@acaprojects.com>
2020-03-25 05:01:04 +1000

    controllers(metadata): get hierarchy n + 1
    Provide endpoints for retrieving all buildings in an org, levels in a
    build and locations in a level.

--- src/controllers/metadata.cr
+++ src/controllers/metadata.cr
@@ -5,6 +5,8 @@ require "../models/level"
 require "../models/location"
 
 module PlaceOS::Analytics
+  # Very quick and dirty mocked metadata service. This should be binned at the
+  # first opportunity.
   class Metadata < Application
     base "/"
 
@@ -15,6 +17,14 @@ module PlaceOS::Analytics
       render json: metadata
     end
 
+    get "/organisation/:id/buildings" do
+      id = params["id"]
+      org = Model::Organisation.find(id)
+      head :not_found unless org
+      buildings = org[:buildings].map &->Model::Building.find(String)
+      render json: buildings
+    end
+
     get "/building/:id" do
       id = params["id"]
       metadata = Model::Building.find id
@@ -22,11 +32,27 @@ module PlaceOS::Analytics
       render json: metadata
     end
 
+    get "/building/:id/levels" do
+      id = params["id"]
+      building = Model::Building.find(id)
+      head :not_found unless building
+      levels = building[:levels].map &->Model::Level.find(String)
+      render json: levels
+    end
+
     get "/level/:id" do
       id = params["id"]
       metadata = Model::Level.find id
       head :not_found unless metadata
       render json: metadata
+    end
+
+    get "/level/:id/locations" do
+      id = params["id"]
+      level = Model::Level.find(id)
+      head :not_found unless level
+      locations = level[:locations].map &->Model::Location.find(String)
+      render json: locations
     end
 
     get "/location/:id" do
