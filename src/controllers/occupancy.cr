ad778bae463d05a45c53a21984ebf79e52fc5666
Kim Burgess <kim@acaprojects.com>
2020-03-26 11:19:48 +1000

    feat(occupancy): stub for grouped requests

--- src/controllers/occupancy.cr
+++ src/controllers/occupancy.cr
@@ -9,12 +9,32 @@ module PlaceOS::Analytics
       start  = Time::Format::RFC_3339.parse params["start"]
       stop   = Time::Format::RFC_3339.parse params["stop"]
       every  = params["every"]?
+      group  = params["group_by"]?
 
       if every
-        locations = Occupancy.series start, stop, every, filters: [
+        head :bad_request if group
+
+        series = Occupancy.series start, stop, every, filters: [
           "(r) => r.org == \"#{org_id}\""
         ]
-        render json: locations
+        render json: series
+
+      elsif group
+        head :bad_request unless group == "type"
+
+        locations = Occupancy.aggregate start, stop, filters: [
+          "(r) => r.org == \"#{org_id}\""
+        ]
+        # FIXME: group location aggregates by types
+        render json: {
+          workstations: 0.0,
+          workpoints: 0.0,
+          informal: 0.0,
+          formal: 0.0,
+          social: 0.0,
+          unknown: locations.values.sum / locations.size.to_f
+        }
+
       else
         locations = Occupancy.aggregate start, stop, filters: [
           "(r) => r.org == \"#{org_id}\""
