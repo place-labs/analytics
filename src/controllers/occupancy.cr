f34f094ff3fe989b9d0918602f81d02ef701b641
Kim Burgess <kim@acaprojects.com>
2020-03-26 14:04:02 +1000

    feat(occupancy): location and level endpoints
    Adds aggregations for buildings and levels.

--- src/controllers/occupancy.cr
+++ src/controllers/occupancy.cr
@@ -5,37 +5,39 @@ module PlaceOS::Analytics
   class Occupancy < Application
     base "/"
 
-    get "/organisation/:id/occupancy" do
-      org_id = params["id"]
-      start  = Time::Format::RFC_3339.parse params["start"]
-      stop   = Time::Format::RFC_3339.parse params["stop"]
-      every  = params["every"]?
-      group  = params["group_by"]?
+    {% for path, tag in { organisation: :org, building: :bld, level: :lvl } %}
+      get "/{{path.id}}/:id/occupancy" do
+        id    = params["id"]
+        start = Time::Format::RFC_3339.parse params["start"]
+        stop  = Time::Format::RFC_3339.parse params["stop"]
+        every = params["every"]?
+        group = params["group_by"]?
 
-      if every
-        head :bad_request if group
-        location_series = Query::Occupancy.series start, stop, every, filters: [
-          "(r) => r.org == \"#{org_id}\""
-        ]
-        head :no_content if location_series.empty?
-        render json: Occupancy.aggregate(location_series)
+        if every
+          head :bad_request if group
+          location_series = Query::Occupancy.series start, stop, every, filters: [
+            "(r) => r.{{tag.id}} == \"#{id}\""
+          ]
+          head :no_content if location_series.empty?
+          render json: Occupancy.aggregate(location_series)
 
-      elsif group
-        head :bad_request unless group == "type"
-        location_aggregates = Query::Occupancy.aggregate start, stop, filters: [
-          "(r) => r.org == \"#{org_id}\""
-        ]
-        head :no_content if location_aggregates.empty?
-        render json: Occupancy.aggregate_by(group, location_aggregates)
+        elsif group
+          head :bad_request unless group == "type"
+          location_aggregates = Query::Occupancy.aggregate start, stop, filters: [
+            "(r) => r.{{tag.id}} == \"#{id}\""
+          ]
+          head :no_content if location_aggregates.empty?
+          render json: Occupancy.aggregate_by(group, location_aggregates)
 
-      else
-        location_aggregates = Query::Occupancy.aggregate start, stop, filters: [
-          "(r) => r.org == \"#{org_id}\""
-        ]
-        head :no_content if location_aggregates.empty?
-        render json: Occupancy.aggregate(location_aggregates)
+        else
+          location_aggregates = Query::Occupancy.aggregate start, stop, filters: [
+            "(r) => r.{{tag.id}} == \"#{id}\""
+          ]
+          head :no_content if location_aggregates.empty?
+          render json: Occupancy.aggregate(location_aggregates)
+        end
       end
-    end
+    {% end %}
 
     # Aggregate a set of single values.
     def self.aggregate(value_hash : Hash(String, Float64))
