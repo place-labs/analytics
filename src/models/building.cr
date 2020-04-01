4214f057e4951a9469f7873b0b5d2c24bfe2215f
Stephen von Takach <steve@advancedcontrol.com.au>
2020-04-01 15:04:17 +1100

    chore(building): update temp data

--- src/models/building.cr
+++ src/models/building.cr
@@ -10,7 +10,17 @@ module PlaceOS::Analytics::Model
           description:  "",
           organisation: "zone-xP3Z77ZH-Z",
           levels:       [
+            "zone-~VdQ7IEvSe",
+            "zone-~Vc7XN6LRz",
             "zone-y3qRLrZGVs",
+            "zone-~VdKS8yX4B",
+            "zone-~VcDEE_EnY",
+            "zone-~VdXyi0ZpC",
+            "zone-~Vd97OUC0h",
+            "zone-~VdFyZwwKq",
+            "zone-~VchiGG_MY",
+            "zone-~Vcwdome1Y",
+            "zone-~Vd1qoDHT0",
           ],
         },
       }
