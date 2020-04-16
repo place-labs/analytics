fde3d6761900777881982ff8ac5771370cd2af65
Kim Burgess <kim@place.technology>
2020-04-16 23:29:47 +1000

    feat(metadata): provide temp dataset for locations

--- src/models/level.cr
+++ src/models/level.cr
@@ -1,142 +1,106 @@
 require "./base/model"
+require "./location"
 
 module PlaceOS::Analytics::Model
   class Level < ModelBase
+    TEMP_DATA = {
+      "zone-~VdQ7IEvSe" => {
+        id:           "zone-~VdQ7IEvSe",
+        name:         "Level 8",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~Vc7XN6LRz" => {
+        id:           "zone-~Vc7XN6LRz",
+        name:         "Level 9",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-y3qRLrZGVs" => {
+        id:           "zone-y3qRLrZGVs",
+        name:         "Level 10",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~VdKS8yX4B" => {
+        id:           "zone-~VdKS8yX4B",
+        name:         "Level 11",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~VcDEE_EnY" => {
+        id:           "zone-~VcDEE_EnY",
+        name:         "Level 12",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~VdXyi0ZpC" => {
+        id:           "zone-~VdXyi0ZpC",
+        name:         "Level 13",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~Vd97OUC0h" => {
+        id:           "zone-~Vd97OUC0h",
+        name:         "Level 14",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~VdFyZwwKq" => {
+        id:           "zone-~VdFyZwwKq",
+        name:         "Level 16",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~VchiGG_MY" => {
+        id:           "zone-~VchiGG_MY",
+        name:         "Level 17",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~Vcwdome1Y" => {
+        id:           "zone-~Vcwdome1Y",
+        name:         "Level 18",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+      "zone-~Vd1qoDHT0" => {
+        id:           "zone-~Vd1qoDHT0",
+        name:         "Level 19",
+        description:  "",
+        organisation: "zone-xP3Z77ZH-Z",
+        building:     "zone-y3qHfB~QrH",
+        locations:    [] of String,
+      },
+    }.tap do |levels|
+      Location::TEMP_DATA.each_value do |location|
+        level = levels[location[:level]]?
+        level[:locations] << location[:id] if level
+      end
+    end
+
     def self.temp_data
-      {
-        "zone-~VdQ7IEvSe" => {
-          id:           "zone-~VdQ7IEvSe",
-          name:         "Level 8",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~Vc7XN6LRz" => {
-          id:           "zone-~Vc7XN6LRz",
-          name:         "Level 9",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-y3qRLrZGVs" => {
-          id:           "zone-y3qRLrZGVs",
-          name:         "Level 10",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~VdKS8yX4B" => {
-          id:           "zone-~VdKS8yX4B",
-          name:         "Level 11",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~VcDEE_EnY" => {
-          id:           "zone-~VcDEE_EnY",
-          name:         "Level 12",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~VdXyi0ZpC" => {
-          id:           "zone-~VdXyi0ZpC",
-          name:         "Level 13",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~Vd97OUC0h" => {
-          id:           "zone-~Vd97OUC0h",
-          name:         "Level 14",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~VdFyZwwKq" => {
-          id:           "zone-~VdFyZwwKq",
-          name:         "Level 16",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~VchiGG_MY" => {
-          id:           "zone-~VchiGG_MY",
-          name:         "Level 17",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~Vcwdome1Y" => {
-          id:           "zone-~Vcwdome1Y",
-          name:         "Level 18",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-        "zone-~Vd1qoDHT0" => {
-          id:           "zone-~Vd1qoDHT0",
-          name:         "Level 19",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          locations:    [
-            "foo",
-            "bar",
-            "baz",
-          ],
-        },
-      }
+      TEMP_DATA
     end
   end
 end
--- src/models/location.cr
+++ src/models/location.cr
@@ -1,46 +1,31 @@
+require "csv"
 require "./base/model"
 
 module PlaceOS::Analytics::Model
   class Location < ModelBase
+    alias Location = {id: String, name: String, description: String, organisation: String, building: String, level: String, type: String?, bookable: Bool, email: String?, capacity: Int32?}
+
+    TEMP_DATA = Hash(String, Location).new.tap do |locations|
+      csv = {{ read_file("src/models/temp_data.csv") }}
+      CSV.new(csv, headers: true) do |csv|
+        entry = {
+          id:           csv["id"],
+          name:         csv["name"],
+          description:  csv["description"],
+          organisation: csv["organisation"],
+          building:     csv["building"],
+          level:        csv["level"],
+          type:         csv["type"]?,
+          bookable:     csv["bookable"] == "TRUE",
+          email:        csv["email"]?,
+          capacity:     csv["capacity"].to_i32?,
+        }
+        locations[entry[:id]] = entry
+      end
+    end
+
     def self.temp_data
-      {
-        "foo" => {
-          id:           "foo",
-          name:         "Foo",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          level:        "zone-y3qRLrZGVs",
-          type:         "Unknown",
-          bookable:     true,
-          email:        "foo@example.com",
-          capacity:     6,
-        },
-        "bar" => {
-          id:           "bar",
-          name:         "Bar",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          level:        "zone-y3qRLrZGVs",
-          type:         "Unknown",
-          bookable:     true,
-          email:        "bar@example.com",
-          capacity:     4,
-        },
-        "baz" => {
-          id:           "baz",
-          name:         "Baz",
-          description:  "",
-          organisation: "zone-xP3Z77ZH-Z",
-          building:     "zone-y3qHfB~QrH",
-          level:        "zone-y3qRLrZGVs",
-          type:         "Unknown",
-          bookable:     true,
-          email:        "baz@example.com",
-          capacity:     3,
-        },
-      }
+      TEMP_DATA
     end
   end
 end
